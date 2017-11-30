642b5c17e94e76084076d6fd9746cec7496f0cd3
Mutai Mwiti <mutaimwiti40@gmail.com>
2017-11-30 09:29:28 +0300

    Add user relationship on blog model

--- app/Blog.php
+++ app/Blog.php
@@ -12,4 +12,8 @@ class Blog extends Model
     protected $fillable = [
         'title', 'body', 'user_id',
     ];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
 }
